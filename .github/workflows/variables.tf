989255fc4f403aa360ac975bc575042cd6c90c15
shriabhc <109714991+shriabhc@users.noreply.github.com>
2026-08-21 15:03:47 +0530

    Update variables.tf

--- .github/workflows/variables.tf
+++ .github/workflows/variables.tf
@@ -1,1 +1,10 @@
+variable "aws_region" {
+  description = "AWS region where the S3 bucket will be created"
+  type        = string
+  default     = "ap-south-1"
+}
 
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
